6e28d2d7f921795a129eb4077d0396f77a864766
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2025-01-21 08:09:05 +0000

    feat[client]: created NotFoundPage component

---
+++ app/news/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NewsNotFoundPage() {
+  return (
+    <div id='error'>
+      <h1>Not found</h1>
+      <p>Unfortunately we couldn't find the requested article</p>
+    </div>
+  );
+}
+
+export default NewsNotFoundPage;
